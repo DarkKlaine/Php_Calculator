90035646624f42edcd2e0c09e7f08da3becca047
darkk <darkklaine@gmail.com>
2023-10-21 11:52:21 +0300

    Refactoring

--- app/CalculatorLogger.php
+++ app/CalculatorLogger.php
@@ -14,50 +14,54 @@ class CalculatorLogger
             return;
         }
 
-        $normalizedLog = '';
+        $this->rawLogProcessing($input, $result);
+        $this->finalLogProcessing();
+    }
+
+    protected function rawLogProcessing($input, $result): void
+    {
+        $existingLog = '';
 
         if (file_exists($this->rawLogPath)) {
-            $normalizedLog = $this->normalizeLogLength(file($this->rawLogPath)) . "\n";
+            $existingLog = $this->trimLogLength(file($this->rawLogPath)) . "\n";
         }
 
         $file = fopen($this->rawLogPath, 'w');
 
-        $textToFile = $normalizedLog . $input . ' = ' . $result . ' | ' . date('Y-m-d H:i:s');
+        $textToFile = $existingLog . $input . ' = ' . $result . ' | ' . date('Y-m-d H:i:s');
 
         fwrite($file, $textToFile);
         fclose($file);
-
-        $this->finalLogProcessing();
     }
 
-    protected function normalizeLogLength(array $textFromFile): string
+    protected function trimLogLength(array $rawLog): string
     {
-        $logLength = count($textFromFile);
+        $logLength = count($rawLog);
         if ($logLength > ($this->maxLogLength - 1)) {
-            array_splice($textFromFile, 0, $logLength - ($this->maxLogLength - 1));
+            array_splice($rawLog, 0, $logLength - ($this->maxLogLength - 1));
         }
-        return implode("", $textFromFile);
+        return implode("", $rawLog);
     }
 
     protected function finalLogProcessing(): void
     {
-        $array = file($this->rawLogPath);
+        $rawLog = file($this->rawLogPath);
         $maxElementLength = 0;
-        for ($i = 0; $i < count($array); $i++) {
-            $elementLength = strlen($array[$i]);
+        for ($i = 0; $i < count($rawLog); $i++) {
+            $elementLength = strlen($rawLog[$i]);
             if ($elementLength > $maxElementLength) {
                 $maxElementLength = $elementLength;
             }
         }
 
-        foreach ($array as $key => &$value) {
-            $number = $key < 9 ? ' ' . ($key + 1) . ' -  ' : ($key + 1) . ' - ';
+        foreach ($rawLog as $key => &$value) {
+            $number = $key < 9 ? ' ' . ($key + 1) . ': ' : ($key + 1) . ':';
             $value = $number . str_repeat(' ', $maxElementLength - strlen($value)) . $value;
         }
         unset($value);
 
         $file = fopen($this->logPath, 'w');
-        fwrite($file, implode("", $array));
+        fwrite($file, implode("", $rawLog));
         fclose($file);
 
     }
