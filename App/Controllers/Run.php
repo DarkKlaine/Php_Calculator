563c6f329999a0a09c41aa282c4dad964d941718
darkk <darkklaine@gmail.com>
2023-10-26 23:02:10 +0300

    добавил .htaccess

--- App/Controllers/Run.php
+++ App/Controllers/Run.php
@@ -1,11 +1,5 @@
 <?php
 
-//TODO Исправить обработку отрицательных значений ввода
-//TODO Исправить вывод если ответ НОЛЬ
-//TODO Apache redirect в index.php
-//TODO сделать BaseController и от него сделать 2 наследника CalculatorController и HistoryController
-//TODO В роутере в зависимости от адреса обращения запускать какой-то из них методом Run
-
 namespace App\Controllers;
 
 use App\Views\CalculatorView;
