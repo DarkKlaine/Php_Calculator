651b5efdc2b12b0213f95baaec89b08adff2117e
darkk <darkklaine@gmail.com>
2023-10-26 18:40:38 +0300

    Логгер

--- App/Models/Computations/Multiply.php
+++ App/Models/Computations/Multiply.php
@@ -7,6 +7,7 @@ class Multiply extends Computation
     public function calculate(): void
     {
         $this->result = (float) $this->value1 * (float) $this->value2;
+        $this->logger->info("Операция умножения: $this->value1 * $this->value2 = $this->result");
     }
 
 }
--- App/Models/Computations/SinCosTan.php
+++ App/Models/Computations/SinCosTan.php
@@ -7,12 +7,23 @@ class SinCosTan extends Computation
 
     public function calculate(): void
     {
-        $this->result = match ($this->action) {
-            "sin" => sin(deg2rad((float)$this->value1)),
-            "cos" => cos(deg2rad((float)$this->value1)),
-            "tan" => tan(deg2rad((float)$this->value1)),
-            default => "Error. Incorrect math.",
-        };
+        switch ($this->action) {
+            case "sin":
+                $this->result = sin(deg2rad((float)$this->value1));
+                $this->logger->info("Операция вычисления синуса угла: $this->value1 sin = $this->result");
+                break;
+            case "cos":
+                $this->result = cos(deg2rad((float)$this->value1));
+                $this->logger->info("Операция вычисления косинуса угла: $this->value1 cos = $this->result");
+                break;
+            case "tan":
+                $this->result = tan(deg2rad((float)$this->value1));
+                $this->logger->info("Операция вычисления тангенса угла: $this->value1 tan = $this->result");
+                break;
+            default:
+                $this->result = "Error. Incorrect math.";
+                break;
+        }
 
     }
 }
