2c4b93d05edba356b0a3a4b3d2c02f03a7bc22bc
darkk <darkklaine@gmail.com>
2023-10-28 21:18:18 +0300

    Сделано: редирект из CalculatorController.php в UIController.php через главную страницу

--- App/Controllers/UIController.php
+++ App/Controllers/UIController.php
@@ -9,10 +9,10 @@ class UIController extends BaseController
 
     public function run(object $serverGlobalDTO): void
     {
-        $post = $serverGlobalDTO->getGet();
+        $get = $serverGlobalDTO->getGet();
 
-        $input = $post['input'] ?? '';
-        $result = $post['result'] ?? '';
+        $input = $get['input'] ?? '';
+        $result = $get['result'] ?? '';
 
         $view = new CalculatorView();
         $view->render($input, $result);
