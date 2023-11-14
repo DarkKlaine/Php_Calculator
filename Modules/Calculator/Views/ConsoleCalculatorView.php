1fd5539109bbfcd4c1a435564589db3190e06a81
darkk <darkklaine@gmail.com>
2023-11-14 20:43:03 +0300

    Навел красоту в консольном выводе

--- Modules/Calculator/Views/ConsoleCalculatorView.php
+++ Modules/Calculator/Views/ConsoleCalculatorView.php
@@ -2,13 +2,13 @@
 
 namespace Modules\Calculator\Views;
 
-use Modules\Calculator\Controllers\IConsoleCalculatorView;
+use Modules\Calculator\Controllers\ConsoleControllers\IConsoleCalculatorView;
 
 class ConsoleCalculatorView implements IConsoleCalculatorView
 {
     public function display(string $input, string $result): void
     {
-        echo "Выражение: $input" . PHP_EOL;
-        echo "Результат: \033[31m$result\033[0m" . PHP_EOL;
+        echo "\033[33mВыражение: $input\033[0m" . PHP_EOL;
+        echo "\033[33mРезультат: $result\033[0m" . PHP_EOL;
     }
 }
--- Modules/Calculator/Views/ConsoleHistoryView.php
+++ Modules/Calculator/Views/ConsoleHistoryView.php
@@ -2,12 +2,13 @@
 
 namespace Modules\Calculator\Views;
 
-use Modules\Calculator\Controllers\IConsoleHistoryView;
+use Modules\Calculator\Controllers\ConsoleControllers\IConsoleHistoryView;
 
 class ConsoleHistoryView implements IConsoleHistoryView
 {
     public function display(string $historyString): void
     {
-        echo $historyString;
+        echo "\033[33mИстория:\033[0m" . PHP_EOL;
+        echo "\033[33m$historyString\033[0m";
     }
 }
