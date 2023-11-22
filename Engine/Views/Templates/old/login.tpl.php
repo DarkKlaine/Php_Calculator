579b7a4cfb3aa068ed3ed5777daa78cf9491d531
darkk <darkklaine@gmail.com>
2023-11-22 22:31:29 +0300

    Перехожу на новый шаблон

--- Engine/Views/Templates/old/login.tpl.php
+++
@@ -1,7 +0,0 @@
-
-<form method="post" action="">
-    <input type="text" name="username" placeholder="Введите логин" required><br>
-    <input type="text" name="password" placeholder="Введите пароль" required><br>
-    <button type="submit">Войти</button>
-</form>
-<br>
