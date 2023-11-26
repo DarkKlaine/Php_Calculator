e5e6690840e2754a8fdbf4153fbc05ea69543902
darkk <darkklaine@gmail.com>
2023-11-26 20:12:44 +0300

    DKE-8 Промежуточный комит

--- Modules/Calculator/Config/Routes/webRoutes.php
+++ Modules/Calculator/Config/Routes/webRoutes.php
@@ -4,7 +4,7 @@ use Modules\Calculator\Controllers\WebControllers\IWebCalculatorController;
 use Modules\Calculator\Controllers\WebControllers\IWebHistoryController;
 
 return [
-    '/calculator' => ['controller' => IWebCalculatorController::class, 'action' => 'showForm'],
+    '/' => ['controller' => IWebCalculatorController::class, 'action' => 'showForm'],
     '/calculate' => ['controller' => IWebCalculatorController::class, 'action' => 'calculate'],
     '/history' => ['controller' => IWebHistoryController::class, 'action' => 'showGeneral'],
     '/session' => ['controller' => IWebHistoryController::class, 'action' => 'showPersonal'],
